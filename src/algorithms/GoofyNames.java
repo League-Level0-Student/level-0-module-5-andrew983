45671d442be2a190310d68ab364a2a1564ae2e4f
League <league@WTS9.local>
2018-11-29 19:55:09 -0800

    goofnames done, guesinpie under work

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -1,4 +1,4 @@
-																																																																																																																											package algorithms;
+package algorithms;
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -18,22 +18,22 @@ public class GoofyNames {
 
 		// 3. Loop through each character of the name (steps 4 - 6).
 		// HINT: Use .length() to determine the number of characters in the String.
-		for (int i = 0; i > 300; i++) {
+		for (int i = 0; i < 300; i++) {
 			// 4. Create a String variable to store the next character of the name
 			// using .substring(start, end)
 			// HINT: replace 'start' and 'end' to get String with character at i
-			
-		String letter = tee.substring(i, i + 1);
+
+			String letter = tee.substring(i, i + 1);
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			// and lower case for ODD characters.
 			if (i % 2 == 0) {
-tee.toUpperCase();
+				tee.toUpperCase();
 			}
 			;
 			// 6. ADD this String (containing 1 char) to the goofyName String
-goofyName += tee;
+			goofyName += tee;
 			// 7. Use pop-up to show user their Goofy name
-
+			System.out.println(goofyName);
 		}
 	}
 }
