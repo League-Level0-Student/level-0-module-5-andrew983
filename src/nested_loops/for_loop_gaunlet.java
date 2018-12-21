4d26e40ea19f79634d94fe8c9f07f3cd57a3b33f
andrew983 <league@iMac-6.attlocal.net>
2018-12-20 20:04:29 -0800

    gaunlet
    s

---
+++ src/nested_loops/for_loop_gaunlet.java
@@ -0,0 +1,30 @@
+package nested_loops;
+
+public class for_loop_gaunlet {
+
+public static void main(String[] args) {
+	
+
+for (int i = 1; i < 500; i++) {
+System.out.println(1+i+"is odd");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//don't touch these!!!
+}
+}
+//don't touch these!!!
