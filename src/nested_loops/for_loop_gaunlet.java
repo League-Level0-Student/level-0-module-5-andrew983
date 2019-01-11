134580d2021c116150367dbeec46d3c99aa1e050
League <league@WTS9.attlocal.net>
2019-01-10 19:56:04 -0800

    for loop class lmost done!
    e

--- src/nested_loops/for_loop_gaunlet.java
+++ src/nested_loops/for_loop_gaunlet.java
@@ -4,26 +4,38 @@ public class for_loop_gaunlet {
 
 public static void main(String[] args) {
 	
-
-for (int i = 1; i < 500; i++) {
-System.out.println(1+i+"is odd");
+/* #7
+for (int i = 2005; i < 2020; i++) {
+	System.out.println("in "+i+ ", I was "+(i-2004));
+*/
+	/*numero 5
+if(i%2 == 1) {
+	System.out.println(i+" is odd");
 }
 
+if(i%2 == 0) {
+	System.out.println(i+" is even");
+}
+*/
+/*
+	
+	if (i%7 == 0) {
+	
+		System.out.println(i+" iS S E V E N");
+	}
+	
+	*/
+	for (int i = 1; i < 10; i++) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		if(i%2 == 0) {
+			System.out.println(i);
+		}
+		
+		
+		
+		
+		
+}
 //don't touch these!!!
 }
 }
