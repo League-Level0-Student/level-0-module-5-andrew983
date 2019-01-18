012cf2d4a32ab6c4661c111e45fe34624aef723f
League <league@WTS9.attlocal.net>
2019-01-17 19:58:07 -0800

    bogies
    e

--- src/nested_loops/for_loop_gaunlet.java
+++ src/nested_loops/for_loop_gaunlet.java
@@ -2,41 +2,46 @@ package nested_loops;
 
 public class for_loop_gaunlet {
 
-public static void main(String[] args) {
-	
-/* #7
-for (int i = 2005; i < 2020; i++) {
-	System.out.println("in "+i+ ", I was "+(i-2004));
-*/
-	/*numero 5
-if(i%2 == 1) {
-	System.out.println(i+" is odd");
-}
+	public static void main(String[] args) {
 
-if(i%2 == 0) {
-	System.out.println(i+" is even");
-}
-*/
-/*
+		/*
+		 * #7 for (int i = 2005; i < 2020; i++) { System.out.println("in "+i+
+		 * ", I was "+(i-2004));
+		 */
+		/*
+		 * numero 5 if(i%2 == 1) { System.out.println(i+" is odd"); }
+		 * 
+		 * if(i%2 == 0) { System.out.println(i+" is even"); }
+		 */
+		/*
+		 * 
+		 * if (i%7 == 0) {
+		 * 
+		 * 
+		 * 
+		 */
 	
-	if (i%7 == 0) {
-	
-		System.out.println(i+" iS S E V E N");
-	}
-	
-	*/
-	for (int i = 1; i < 10; i++) {
+		
+		for (int i = 0; i < 3; i++) {
 
-		if(i%2 == 0) {
-			System.out.println(i);
-		}
+			
+
+			for (int h = 0; h < 0; h++) {
+				
+
+				for (int L = 0; L < 0; L++) {
+		
+		
+					System.out.println(i+ " "+h+ " "+L);
 		
 		
 		
 		
 		
+			}
+			}
+		
+// don't touch these!!!
 }
-//don't touch these!!!
-}
-}
-//don't touch these!!!
+	}}
+// don't touch these!!!
